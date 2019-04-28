a7d53bbb5cdc26dd2a72619346524cf6e6888fbd
walle <michelle210181@gmail.com>
2019-04-28 20:28:04 +0800

    Add edit page

--- routes/record.js
+++ routes/record.js
@@ -2,19 +2,40 @@ const express = require("express");
 const router = express.Router();
 const Record = require("../models/record");
 
+// 新增頁面
 router.get("/new", (req, res) => {
   res.render("new");
 });
 
+// 儲存新增
 router.post("/", (req, res) => {
-  const record = Record({
-    name: req.body.name,
-    userId: req.user._id
-  });
+  const record = Record(req.body);
+  console.log(req.body);
 
   record.save(err => {
     if (err) return console.log(err);
     return res.redirect("/");
+  });
+});
+
+// 編輯頁面
+router.get("/:id/edit", (req, res) => {
+  Record.findById({ _id: req.params.id }, (err, record) => {
+    if (err) return console.error(err);
+    return res.render("edit", { record });
+  });
+});
+
+// 儲存編輯
+router.post("/", (req, res) => {
+  Record.findById({ _id: req.params.id }, (err, record) => {
+    if (err) return console.error(err);
+    Object.assign(record, req.body);
+
+    record.save(err => {
+      if (err) return console.error(err);
+      return res.redirect("/");
+    });
   });
 });
 
