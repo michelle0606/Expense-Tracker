af9eea8c64fe08fd52c2a7b5e8f352f5a1f2cba7
walle <michelle210181@gmail.com>
2019-05-24 14:04:28 +0800

    refactor /routes/home.js

--- routes/home.js
+++ routes/home.js
@@ -1,71 +1,60 @@
 // 首頁
 
-const express = require("express");
-const router = express.Router();
-const Record = require("../models/record");
-const { authenticated } = require("../config/auth");
+const express = require('express')
+const router = express.Router()
+const Record = require('../models/record')
+const { authenticated } = require('../config/auth')
 
-router.get("/", authenticated, (req, res) => {
-  const start = req.query.month ? new Date() : false;
-  const end = req.query.month ? new Date() : false;
-  const category = req.query.category ? req.query.category : { $exists: true };
-  if (start && end) {
-    start.setMonth(Number(req.query.month - 1), [1]);
-    end.setMonth(Number(req.query.month - 1), [31]);
-    start.setHours(0);
-    end.setHours(0);
-    start.setMinutes(0);
-    end.setMinutes(0);
-    date = {
-      $gte: start,
-      $lte: end
-    };
-  } else {
-    date = { $exists: true };
-  }
+router.get('/', authenticated, (req, res) => {
+  const year = new Date().getFullYear()
+  const month = Number(req.query.month - 1)
+  const start = req.query.month ? new Date(Date.UTC(year, month, 1, 0)) : false
+  const end = req.query.month ? new Date(Date.UTC(year, month, 31, 0)) : false
+  const category = req.query.category ? req.query.category : { $exists: true }
+  const date = start && end ? { $gte: start, $lte: end } : { $exists: true }
 
   Record.find({ userId: req.user._id, category, date })
     .exec()
     .then(records => {
-      let total = 0;
+      let total = 0
       records = records.map(record => {
-        total += record.amount;
-        setIcon(record);
-        return record;
-      });
-      res.render("index", {
+        total += record.amount
+        setIcon(record)
+        return record
+      })
+      res.render('index', {
         records,
         total,
         select: { month: req.query.month, category: req.query.category }
-      });
+      })
     })
     .catch(err => {
-      console.log(err);
-    });
-});
+      console.log(err)
+    })
+})
 
-module.exports = router;
+module.exports = router
 
 function setIcon(record) {
   switch (record.category) {
-    case "家居物業":
-      record.category = "fas fa-home";
-      break;
-    case "交通出行":
-      record.category = "fas fa-shuttle-van";
-      break;
-    case "休閒娛樂":
-      record.category = "fas fa-grin-beam";
-      break;
-    case "餐飲食品":
-      record.category = "fas fa-utensils";
-      break;
-    case "其它":
-      record.category = "fas fa-pen";
-      break;
+    case '家居物業':
+      record.category = 'fas fa-home'
+      break
+    case '交通出行':
+      record.category = 'fas fa-shuttle-van'
+      break
+    case '休閒娛樂':
+      record.category = 'fas fa-grin-beam'
+      break
+    case '餐飲食品':
+      record.category = 'fas fa-utensils'
+      break
+    case '其它':
+      record.category = 'fas fa-pen'
+      break
     default:
-      break;
+      break
   }
 }
 
-module.exports = router;
+module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,72 +1,65 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/user");
-const passport = require("passport");
-const bcrypt = require("bcryptjs");
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 // 登入頁面
-router.get("/login", (req, res) => {
-  res.render("login");
-});
+router.get('/login', (req, res) => {
+  res.render('login')
+})
 
 // 登入檢查
-router.post("/login", (req, res, next) => {
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/user/login"
-  })(req, res, next);
-});
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/user/login'
+  })(req, res, next)
+})
 
 // 註冊頁面
-router.get("/register", (req, res) => {
-  res.render("register");
-});
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 
 // 註冊檢查
-router.post("/register", (req, res) => {
-  const { name, email, password, password2 } = req.body;
+router.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body
   User.findOne({ email: email }).then(user => {
-    if (user) {
-      res.render("register", {
+    if (user || password !== password2) {
+      res.render('register', {
         name,
         email,
         password,
         password2
-      });
-    } else if (password !== password2) {
-      res.render("register", {
-        name,
-        email,
-        password,
-        password2
-      });
+      })
     } else {
       const newUser = new User({
         name,
         email,
         password
-      });
+      })
       // 密碼雜揍
       bcrypt.genSalt(10, (err, salt) =>
         bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
+          if (err) throw err
+          newUser.password = hash
           newUser
             .save()
-            .then(user => {
-              res.redirect("/");
+            .then(() => {
+              res.redirect('/')
             })
-            .catch(err => console.log(err));
+            .catch(err => console.log(err))
         })
-      );
+      )
     }
-  });
-});
+  })
+})
 
 // 登出
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/user/login");
-});
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/user/login')
+})
 
-module.exports = router;
+module.exports = router
